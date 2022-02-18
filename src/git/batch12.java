53c4ebcd265f545640c1663f0e969146de792432
HILMI <hlmozcelik1@gmail.com>
2022-02-18 14:27:38 +0300

    some code updates

--- src/git/batch12.java
+++ src/git/batch12.java
@@ -5,7 +5,7 @@ public class batch12 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("we are were");
-		
+		System.out.println("we ere");
 
 	}
 
